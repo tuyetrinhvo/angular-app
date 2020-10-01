0eb1171442cdd2c0de742b645d13a414c919d99c
Tuyetrinh VO <tuyetrinhvo@gmail.com>
2020-10-01 18:57:12 +0200

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'angular-app';
+	isAuth = false;
+
+	constructor() {
+		setTimeout(() => {
+			this.isAuth = true;
+		}, 2000);
+	}
 }
