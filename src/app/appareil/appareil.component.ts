8d0612317e4f9b5ca1cba0fc5932661de64ca1e7
Tuyetrinh VO <tuyetrinhvo@gmail.com>
2020-10-01 19:27:05 +0200

    use decorator @Input()

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-appareil',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: [ './appareil.component.scss' ]
 })
 export class AppareilComponent implements OnInit {
-	appareilName: string = 'Machine à laver';
+	@Input() appareilName: string;
 	appareilStatus: string = 'éteint';
 
 	constructor() {}
