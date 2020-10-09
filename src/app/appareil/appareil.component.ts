30f4f99d197105a85e1a03ac70e55349244b3a41
Tuyetrinh VO <tuyetrinhvo@gmail.com>
2020-10-09 14:10:55 +0200

    use Observable 03 with  unsubscribe()

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,34 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AppareilService } from '../services/appareil.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
 	selector: 'app-appareil',
 	templateUrl: './appareil.component.html',
 	styleUrls: ['./appareil.component.scss']
 })
-export class AppareilComponent implements OnInit {
+export class AppareilComponent implements OnInit, OnDestroy {
 
 	appareils: any[];
 	lastUpdate = new Date();
 
 	seconds: number;
+	counterSubscription: Subscription;
 
 	constructor(private appareilSerivce: AppareilService) { }
 
 	ngOnInit(): void {
 		this.appareils = this.appareilSerivce.appareils;
 		const counter = Observable.interval(1000);
-		counter.subscribe(
+		this.counterSubscription = counter.subscribe(
 			(data: number) => {
 				this.seconds = data;
-			},
-			(error: any) => {
-				console.log('error');
-			},
-			() => {
-				console.log('Observable complete');
 			}
 		);
 
@@ -40,6 +36,10 @@ export class AppareilComponent implements OnInit {
 
 	onToutEteindre() {
 		this.appareilSerivce.switchOffAll();
+	}
+
+	ngOnDestroy() {
+		this.counterSubscription.unsubscribe();
 	}
 
 }
