e0e8955491557cc48c6f25536b7a16c924ac4acd
Tuyetrinh VO <tuyetrinhvo@gmail.com>
2020-10-09 13:56:05 +0200

    use Observable 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = 'angular-app';
+
+	seconds: number;
+
+	constructor() { }
+
+	ngOnInit() {
+		const counter = Observable.interval(1000);
+		counter.subscribe(
+			() => { },
+			() => { },
+			() => { }
+		);
+	}
+
+
 }
